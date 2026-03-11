c4707b59cec3046f4cd5ba3e0e31b6b20e064a3c
Santino Delmonte <142530915+santinodelmonte@users.noreply.github.com>
2026-03-11 12:22:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+new Typed(".typing", {
+strings: [
+"Desarrollador Full Stack",
+"Apasionado por el Backend",
+"Creando soluciones digitales"
+],
+typeSpeed:60,
+backSpeed:40,
+loop:true
+});
+
+particlesJS("particles-js",{
+particles:{
+number:{value:40},
+size:{value:3},
+move:{speed:1},
+line_linked:{enable:true}
+}
+});
